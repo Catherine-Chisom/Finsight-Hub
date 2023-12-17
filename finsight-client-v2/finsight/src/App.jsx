3cff71515d03c9a57e92e7fdda95396b4e3423b5
jesse <jessebett1999@gmail.com>
2023-12-17 08:26:29 +0300

    initial commit client

---
+++ finsight-client-v2/finsight/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css';
+// import Graph from './components/Graph';
+// import Form from './components/Form';
+
+function App() {
+  return (
+  <div className="App">
+    <div className="container  max-w-6xl  mx-auto text-center text-pink-800 drop-shadow-lg ">
+      <h1 className="text-2xl py-8 mb-10 bg-slate-800 text-white rounded">Expense Tracker</h1>
+
+      {/* grid columns */}
+      <div className="grid md:grid-cols-2 gap-4">
+          Chart
+         
+      </div>
+    </div>
+  </div>
+  );
+}
+
+export default App;
