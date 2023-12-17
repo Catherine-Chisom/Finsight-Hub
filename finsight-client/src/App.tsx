205476eb1839f74001308a63a39fedf744282c32
jesse <jessebett1999@gmail.com>
2023-12-17 07:15:41 +0300

    client side setup

---
+++ finsight-client/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <p className=' text-red-200'>Fintech client </p>
+        </div>
+    </>
+  )
+}
+
+export default App
